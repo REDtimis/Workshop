253c4c55c1e1f7a307672b26ffe36056e896f004
REDtimis <93532438+REDtimis@users.noreply.github.com>
2026-02-18 23:14:46 +0100

    Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -8,3 +8,19 @@ with open("example.txt", "r") as file:
     content = file.read()
     print("TXT File Content:")
     print(content)
+    
+import csv
+
+# Write CSV file
+with open("students.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Name", "Age"])
+    writer.writerow(["Ali", 25])
+    writer.writerow(["Sara", 22])
+
+# Read CSV file
+with open("students.csv", "r") as file:
+    reader = csv.reader(file)
+    print("CSV File Content:")
+    for row in reader:
+        print(row)
